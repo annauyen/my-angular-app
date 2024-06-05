9ed708d64039ed90dc20bf5e51cf500f47ae603f
JoeyUyen <nhiuyenb@gmail.com>
2024-06-05 17:15:31 +0700

    modify header components

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"; // to convert the code to the component
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css'
+})
+export class HeaderComponent {}
