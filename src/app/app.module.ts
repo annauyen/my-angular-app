d54c2fd6a0f4cde2bd03ca9e7ef7605b4e5c754e
bnnuyen <nhiuyenb@gmail.com>
2024-06-10 09:24:16 +0700

    migrate to modules - 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
 import { TasksModule } from './tasks/tasks.module';
 
